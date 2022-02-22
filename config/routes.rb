c728349fbe2e4cae4a07b97c94e60b96594dcf64
TakKokubu <kokubu.tk@gmail.com>
2022-02-22 10:17:31 +0900

    create-memo-show-page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth'
     resources :users
-    resources :memos, only: [:index, :create, :destroy] do
+    resources :memos, only: [:index, :create, :destroy, :show] do
       resource :favorites, only: [:create, :destroy]
     end
   end
